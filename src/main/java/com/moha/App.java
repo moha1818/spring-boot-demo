41f80db749c15b487c1743d80072155815d69410
maozewei <maozewei799845210@qq.com>
2017-09-07 15:48:20 +0800

    ignore

--- src/main/java/com/moha/App.java
+++ src/main/java/com/moha/App.java
@@ -2,12 +2,14 @@ package com.moha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@ServletComponentScan
 public class App 
 {
     public static void main( String[] args )
--- src/main/java/com/moha/controller/HelloWorldControl.java
+++ src/main/java/com/moha/controller/HelloWorldControl.java
@@ -5,9 +5,11 @@ package com.moha.controller;
 import com.moha.domain.Demo;
 import com.moha.domain.DemoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloWorldControl {
@@ -16,12 +18,16 @@ public class HelloWorldControl {
     private DemoRepository demoRepository;
 
     @RequestMapping("/hello")
-    public Demo helloword(@RequestParam Integer id){
-       /* Demo demo = new Demo();
-        demo.setId(1);
-        demo.setName("mao1");
-        demoRepository.save(demo);*/
-
-        return demoRepository.findById(id);
+    @Cacheable(value="user-key")
+    public List<Demo> helloword(){
+        return demoRepository.findAll();
     }
+
+    @RequestMapping("/savehello")
+    public Demo insterthelloword(){
+        Demo demo = new Demo();
+        demo.setName("mao1");
+        return demoRepository.save(demo);
+    }
+
 }
